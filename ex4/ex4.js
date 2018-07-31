518c4c29c82ef82ce0fb91faa7bec27b36bf4458
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-07-31 13:06:39 -0700

    master and commander of tag functions
    converts values to uppercase

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,10 +1,19 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	let result = '';
+	  for(let i = 0; i < strings.length; i+=1) {
+		result += strings[i];
+			if(values[i] !== undefined) {
+				result += values[i].toUpperCase();			
+			}
+ 	 }
+	return result;
+}
 
 var name = "kyle",
 	twitter = "getify",
 	classname = "es6 workshop";
 
 console.log(
-	`Hello ____ (@____), welcome to the ____!` ===
+	upper`Hello ${name} (@${twitter}), welcome to the ${classname}!` ===
 	"Hello KYLE (@GETIFY), welcome to the ES6 WORKSHOP!"
 );
